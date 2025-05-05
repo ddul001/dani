3bb2ff2bc60e5718cff16df13d3d6a85e74f4f0e
Your Name <you@example.com>
2025-05-05 18:28:21 +0200

    Setup project scaffold

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,13 +1,1 @@
-from .browsing_tool    import BrowsingTool
-from .scraping_tool    import ScrapingTool
-from .calculation_tool import CalculationTool
-from .rest_api_tool    import RestApiTool
-from .autopilot_tool   import AutopilotTool
-
-__all__ = [
-    "BrowsingTool",
-    "ScrapingTool",
-    "CalculationTool",
-    "RestApiTool",
-    "AutopilotTool",
-]
+from tools import BrowsingTool, ScrapingTool, CalculationTool, RestApiTool, AutopilotTool
