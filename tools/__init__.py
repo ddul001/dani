7c2c661ec2fe483ecbbde86e43ddea085b3e90be
Your Name <you@example.com>
2025-05-05 18:29:49 +0200

    Setup project scaffold

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,1 +1,5 @@
-from tools import BrowsingTool, ScrapingTool, CalculationTool, RestApiTool, AutopilotTool
+from .browsing_tool import BrowsingTool
+from .scraping_tool import ScrapingTool
+from .calculation_tool import CalculationTool
+from .rest_api_tool import RestApiTool
+from .autopilot_tool import AutopilotTool
